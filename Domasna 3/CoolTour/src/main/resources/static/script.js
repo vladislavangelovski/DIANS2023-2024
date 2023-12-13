d9083391f095aed7079950e6a744ce339c7432e7
EvgenijWasTaken <evgenij.anusev@outlook.com>
2023-12-13 17:14:08 +0100

    Animations/UI Update

--- Domasna 3/CoolTour/src/main/resources/static/script.js
+++ Domasna 3/CoolTour/src/main/resources/static/script.js
@@ -30,5 +30,41 @@ $(document).ready(function() {
     });
 });
 
+function Animacija(){
+
+    var d=document.getElementById('object');
+    d.style.backgroundColor="rgba(0, 0, 0, 0)";
+
+
+    var opacity=0;
+    function AddOpacity(){
+       opacity=opacity+0.05;
+       d.style.border="solid 1px";
+        d.style.borderColor=`rgba(0,0,0,${opacity})`;
+        d.style.backgroundColor=`rgba(255,255,255,${opacity})`;
+        var c = d.childNodes;
+        for(let i=0;i<c.length;i++){
+            c[i].style.color=`rgba(0,0,0,${opacity})`;
+            c[i].style.backgroundColor=`rgba(240, 255, 254 ,${opacity})`;
+            c[i].style.border="solid 1px";
+            c[i].style.borderColor=`rgba(0,0,0,${opacity})`;
+        }
+        if(opacity>=0.85){
+
+            var c = d.childNodes;
+            for(let i=0;i<c.length;i++){
+
+                c[i].style.color=`rgba(0,0,0,1)`;
+
+            }
+            clearInterval(ID);
+        }
+    }
+
+   var ID=setInterval(AddOpacity,35);
+
+}
+
+
 
 
